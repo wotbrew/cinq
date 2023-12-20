b85e9af78fde6e5de5b5c482ac3de1cd0f52abc5
Dan Stone <wotbrew@gmail.com>
2023-12-20 23:00:39 +0000

    Slow seq RA

--- src/com/wotbrew/cinq.clj
+++ src/com/wotbrew/cinq.clj
@@ -1,6 +1,121 @@
-(ns com.wotbrew.cinq)
+(ns com.wotbrew.cinq
+  (:import (java.util ArrayList HashMap)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defprotocol Relation
+  :extend-via-metadata true
+  (columns [rel])
+  (tuple-seq [rel]))
+
+(defn relation? [x]
+  (or (and (contains? (meta x) `tuple-seq)
+           (contains? (meta x) `columns))
+      (satisfies? Relation x)))
+
+(defn arity [rel] (count (columns rel)))
+
+(defn- join-tuples [l r added-col-count]
+  (into l (take added-col-count) (concat r (repeat nil))))
+
+(defn nested-loop-join [rel added-col-count f]
+  (with-meta
+    (for [l (tuple-seq rel)
+          r (tuple-seq (f l))]
+      (join-tuples l r added-col-count))
+    {`tuple-seq identity
+     `columns (constantly (into (columns rel) (repeat added-col-count :cinq/anon)))}))
+
+(defn hash-equi-join
+  [build
+   build-key-fn
+   probe
+   probe-key-fn
+   added-col-count
+   theta-condition]
+  (-> (lazy-seq
+        (let [hm (HashMap.)
+              _ (doseq [b (tuple-seq build)]
+                  (let [k (build-key-fn b)
+                        nested-list (.get hm k)
+                        empty-nested-list (when-not nested-list (ArrayList.))
+                        _ (when empty-nested-list (.put hm k empty-nested-list))
+                        nested-list (or nested-list empty-nested-list)]
+                    (.add nested-list b)))]
+          (for [p (tuple-seq probe)
+                b (.get hm (probe-key-fn p))
+                :when (theta-condition b p)]
+            (join-tuples b p added-col-count))))
+      (with-meta
+        {`tuple-seq identity
+         `columns (constantly (into (columns build) (repeat added-col-count :cinq/anon)))})))
+
+(defn hash-equi-left-join
+  [probe
+   probe-key-fn
+   build
+   build-key-fn
+   added-col-count
+   theta-condition]
+  (-> (lazy-seq
+        (let [hm (HashMap.)
+              _ (doseq [b (tuple-seq build)]
+                  (let [k (build-key-fn b)
+                        nested-list (.get hm k)
+                        empty-nested-list (when-not nested-list (ArrayList.))
+                        _ (when empty-nested-list (.put hm k empty-nested-list))
+                        nested-list (or nested-list empty-nested-list)]
+                    (.add nested-list b)))]
+          (for [p (tuple-seq probe)
+                :let [bs (filter #(theta-condition p %) (.get hm (probe-key-fn p)))]
+                b (if (seq bs) bs (repeat added-col-count nil))]
+            (join-tuples p b added-col-count))))
+      (with-meta
+        {`tuple-seq identity
+         `columns (constantly (into (columns probe) (repeat added-col-count :cinq/anon)))})))
+
+(defn nested-loop-left-join [rel added-col-count f]
+  (with-meta
+    (for [l (tuple-seq rel)
+          :let [rs (tuple-seq (f l))]
+          r (if (seq rs) rs [(repeat nil)])]
+      (join-tuples l r added-col-count))
+    {`tuple-seq identity
+     `columns (constantly (into (columns rel) (repeat added-col-count :cinq/anon)))}))
+
+(defn where [rel f]
+  (with-meta (lazy-seq (filter f (tuple-seq rel))) (meta rel)))
+
+(defn group [rel & group-keys]
+  (let [group-key (if (seq group-keys) (apply juxt group-keys) (constantly []))
+        arity (arity rel)]
+    (with-meta
+      (for [[gk ts] (group-by group-key (tuple-seq rel))]
+        (into (mapv (fn [i] (map (fn [t] (nth t i)) ts)) (range arity)) gk))
+      {`tuple-seq identity
+       `columns (constantly (into (columns rel) (repeat (count group-keys) :cinq/anon)))})))
+
+(defn order [rel & order-clauses]
+  rel)
+
+(defn limit [rel n]
+  (with-meta (lazy-seq (take n (tuple-seq rel))) (meta rel)))
+
+(defn add [rel & fns]
+  (let [f (fn [t] (reduce (fn [t f] (conj t (f t))) t fns))]
+    (with-meta
+      (lazy-seq (map f (tuple-seq rel)))
+      {`tuple-seq identity
+       `columns (constantly (into (columns rel) (repeat (count fns) :cinq/anon)))})))
+
+(defn select [rel & fns]
+  (let [f (if (seq fns) (apply juxt fns) (constantly []))]
+    (with-meta
+      (lazy-seq (map f (tuple-seq rel)))
+      {`tuple-seq identity
+       `columns (constantly (vec (repeat (count fns) :cinq/anon)))})))
+
+(defn scan [x cols]
+  (let [tuple-f (if (seq cols) (apply juxt (map (fn [col] (if (= :cinq/self col) identity col)) cols)) (constantly []))]
+    (with-meta
+      (map tuple-f x)
+      {`tuple-seq identity
+       `columns (constantly (vec cols))})))
--- test/com/wotbrew/cinq_test.clj
+++ test/com/wotbrew/cinq_test.clj
@@ -1,3 +1,41 @@
 (ns com.wotbrew.cinq-test
   (:require [clojure.test :refer :all]
             [com.wotbrew.cinq :refer :all]))
+
+(deftest arity-test
+  (are [rel expected] (= expected (arity rel))
+
+    (scan nil []) 0
+    (scan nil [:foo]) 1
+    (scan nil [:foo, :bar]) 2
+
+    (nested-loop-join (scan nil [:foo]) 2 (fn [_])) 3))
+
+(deftest relation?-test
+  (are [x expected] (= expected (relation? x))
+
+    nil false
+    [] false
+    [1 2] false
+    {} false
+    () false
+    (map inc [1 2]) false
+
+    (scan nil []) true
+    (where (scan nil []) (fn [_])) true))
+
+(deftest customers-example
+  (let [customers [{:customer-id 0, :firstname "Jane", :lastname "Doe"}
+                   {:customer-id 1, :firstname "John", :lastname "Smith"}]]
+
+    (are [rel expected] (= expected (tuple-seq rel))
+
+      (scan customers []) [[], []]
+      (scan customers [:cinq/self]) (map vector customers)
+      (scan customers [:firstname]) [["Jane"] ["John"]]
+      (scan customers [:firstname :lastname]) [["Jane" "Doe"] ["John" "Smith"]]
+
+      (-> customers
+          (scan [:firstname :lastname])
+          (where (fn [{ln 1}] (= ln "Doe"))))
+      [["Jane" "Doe"]])))
