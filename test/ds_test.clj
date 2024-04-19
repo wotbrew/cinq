41e36b64607702eb1a069f195b05d447bb03330a
Dan Stone <danielstone@riverford.co.uk>
2024-04-19 13:05:21 +0100

    Add data(script/levin) style test examples. Look ma, no indexes!

--- test/ds_test.clj
+++ test/ds_test.clj
@@ -1,15 +1,14 @@
 (ns ds-test
   (:require [clojure.test :refer :all]
-            [com.wotbrew.cinq.array-seq :as aseq]
-            [com.wotbrew.cinq.row-major :as rm]))
+            [com.wotbrew.cinq :as c]))
 
 (def next-eid (volatile! 0))
 
-(defrecord Man [id name last-name sex ^long age ^long salary])
+(defrecord Person [^long id name last-name sex ^long age ^long salary])
 
 (defn random-man []
-  (map->Man
-    {:id (str (vswap! next-eid inc))
+  (map->Person
+    {:id (vswap! next-eid inc)
      :name (rand-nth ["Ivan" "Petr" "Sergei" "Oleg" "Yuri" "Dmitry" "Fedor" "Denis"])
      :last-name (rand-nth ["Ivanov" "Petrov" "Sidorov" "Kovalev" "Kuznetsov" "Voronoi"])
      :sex (rand-nth [:male :female])
@@ -20,30 +19,46 @@
 (def people20k (vec (shuffle (take 20000 people))))
 
 (defn q1 []
-  (aseq/q* [p people20k
-            :when (= "Ivan" p:name)]
-           p:id))
-
-(defn q1-rm []
-  (rm/q [^Man p people20k
-         :when (= "Ivan" p:name)]
-        p:id))
+  (c/q [^Person p people20k
+        :when (= "Ivan" p:name)]
+       p:id))
 
 (defn qpred1 []
-  (aseq/q* [^Man p people20k
-            :when (> p:salary 50000)]
-           p:id))
+  (c/q [^Person p people20k
+        :when (> p:salary 50000)]
+       p:id))
 
-(defn qpred1-rm []
-  (rm/q [^Man p people20k
-          :when (> p:salary 50000)]
-         p:id))
+(defn q2 []
+  (c/q [^Person p people20k
+        :when (= "Ivan" p:name)]
+    (c/tuple :id p:id :age p:age)))
+
+(defn q3 []
+  (c/q [^Person p people20k
+        :when (and (= "Ivan" p:name) (= :male p:sex))]
+    (c/tuple :id p:id :last-name p:last-name)))
+
+(defn q4 []
+  (c/q [^Person p people20k
+        :when (and (= "Ivan" p:name) (= :male p:sex))]
+    (c/tuple :id p:id :last-name p:last-name, :age p:age)))
+
+(defn q5 []
+  (c/q [^Person p people20k
+        ^Person p1 people20k
+        :when (and (= "Ivan" p:name)
+                   (= p:age p1:age))]
+    (c/tuple :id p1:id :last-name p1:last-name, :age p1:age)))
 
 (comment
 
   (require 'criterium.core)
-  (criterium.core/quick-bench (com.wotbrew.cinq.tpch-test/iter-count (q1)))
-  (criterium.core/quick-bench (com.wotbrew.cinq.tpch-test/iter-count (q1-rm)))
-  (criterium.core/quick-bench (com.wotbrew.cinq.tpch-test/iter-count (qpred1)))
-  (criterium.core/quick-bench (com.wotbrew.cinq.tpch-test/iter-count (qpred1-rm)))
+  (criterium.core/quick-bench (q1))
+  (criterium.core/quick-bench (q2))
+  (criterium.core/quick-bench (q3))
+  (criterium.core/quick-bench (q4))
+  (clj-async-profiler.core/profile
+    (criterium.core/quick-bench (q5))
+    )
+  (criterium.core/quick-bench (qpred1))
   )
