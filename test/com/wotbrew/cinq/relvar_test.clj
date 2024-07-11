62ea6a9909a5099f938df3023a345ff79c4dffd9
Dan Stone <danielstone@riverford.co.uk>
2024-07-11 10:54:54 +0100

    Small improvements to relvar test

--- test/com/wotbrew/cinq/relvar_test.clj
+++ test/com/wotbrew/cinq/relvar_test.clj
@@ -6,10 +6,10 @@
   (let [rv1 (c/relvar)
         rv2 (c/relvar)]
 
-    (c/rel-set rv1 [1, 2, 3, 4])
-    (c/rel-set rv2 [2, 4, 5, 6])
+    (c/rel-set rv1 [1 2 3 4])
+    (c/rel-set rv2 [2 4 5 6])
 
-    (is (= [2, 4] (vec (c/q [x rv1
+    (is (= [2 4] (vec (c/q [x rv1
                              y rv2
                              :when (= x y)]
                          x))))
@@ -17,15 +17,15 @@
     (is (= 4 (c/insert rv1 5)))
     (is (= 5 (c/insert rv1 5)))
 
-    (is (= [1, 2, 3, 4, 5, 5] (vec rv1)))
+    (is (= [1 2 3 4 5 5] (vec rv1)))
 
     (c/delete-where rv1 [x] (= x 42))
 
-    (is (= [1, 2, 3, 4, 5, 5] (vec rv1)))
+    (is (= [1 2 3 4 5 5] (vec rv1)))
 
     (c/delete-where rv1 [x] (= x 5))
 
-    (is (= [1, 2, 3, 4] (vec rv1)))
+    (is (= [1 2 3 4] (vec rv1)))
 
     (c/update-where rv1 [x] (even? x) 42)
 
@@ -35,8 +35,11 @@
       (try
         (dosync
           (c/insert rv1 3)
+          (c/rel-set rv2 [])
           (is (= {1 1, 42 2, 3 2} (frequencies rv1)))
+          (is (= [] (vec rv2)))
           (throw (ex-info "fail tx" {})))
         (catch Throwable _))
 
-      (is (= {1 1, 42 2, 3 1} (frequencies rv1))))))
+      (is (= {1 1, 42 2, 3 1} (frequencies rv1)))
+      (is (= [2 4 5 6] (vec rv2))))))
